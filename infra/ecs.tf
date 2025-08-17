bb95b36f15142289da8ac4f8e0ba64be0dae0752
hibye0714 <48339717+YongJian-YJ@users.noreply.github.com>
2025-08-17 18:57:51 +0800

    Update ecs.tf

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -78,7 +78,7 @@ resource "aws_lb_target_group" "frontend_tg" {
   health_check {
     enabled             = true
     healthy_threshold   = 2
-    unhealthy_threshold = 3
+    unhealthy_threshold = 10
     timeout             = 5
     interval            = 30
     path                = "/"
